68a9acc9c35054f3e691cd601ac8c4a477b8828d
hanh <dovanhanh12b@gmail.com>
2019-08-03 21:50:33 +0700

    udpate 03.08.19

--- index.php
+++ index.php
@@ -1,50 +1,25 @@
 <?php
-
 function isPrime($number)
 {
     if ($number < 2) {
         return false;
     }
+
     for ($i = 2; $i <= sqrt($number); $i++) {
         if ($number % $i == 0) {
             return false;
         }
     }
+
     return true;
 }
 
-function printNumber($n)
-{
-    for ($i = 0; $i < $n; $i++) {
-        if (isPrime($i)) {
-            echo $i . ' ';
+function checkAmount($amount){
+    for ($i=0;$i<$amount;$i++){
+        if (isPrime($i)){
+            echo $i.' ';
         }
     }
 }
 
-?>
-
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>Print Prime Number</title>
-
-</head>
-<body>
-<form method="post">
-
-    <label>Enter amount number you want</label>
-    <input type="text" name="number">
-    <button type="submit">Display</button>
-
-    <?php
-    if ($_SERVER['REQUEST_METHOD'] = 'POST') {
-        $number = $_POST['number'];
-
-        printNumber($number);
-    }
-    ?>
-</form>
-
-</body>
-</html>
+checkAmount(50);
